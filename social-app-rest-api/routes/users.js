7374ff791a71e4e677ed217932397f78a6c395d5
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-02-03 01:13:30 +0530

    update

---
+++ social-app-rest-api/routes/users.js
@@ -0,0 +1,73 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.get("/", (req, res) => {
+    res.send("hey its user route")
+});
+
+//update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userId == req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("account has been updated")
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("you can update only your account");
+    }
+})
+//delete user
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId == req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("account has been deleted");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("you can delete only your account");
+    }
+})
+//get a user
+router.get("/:id", async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt,...other} = user._doc
+        res.status(200).json(other);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+//follow a user
+// router.put("/:id/follow", async (req,res)=>{
+//     if(req.body.userId != req.params.id){
+//         try{
+//             const user = await User.findById(req.params.id);
+            
+//         }catch(err){
+//             res.status(500).json(err);
+//         }
+//     }else{
+//         res.status(403).json("you cant follow yourself");
+//     }
+// })
+
+//unfobllow a user
+
+
+module.exports = router;

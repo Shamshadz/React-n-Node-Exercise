b1eedb5e5b44c5f9aa0b5d7358f2c145dfa00158
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-02-08 19:04:19 +0530

    update

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Signup from "./pages/register/Signup";
 
 function App() {
   return (
-    <Home/>
+    <Profile/>
   );
 }
 
